ef2df4d28412229bcc403c39ecf6692442a9d757
Chris-G-5150 <distort_everything@proton.me>
2024-01-02 15:18:16 +0000

    updated user-auth

--- components/SignUp.js
+++ components/SignUp.js
@@ -1,106 +1,141 @@
-import { useState } from "react"
-import { StyleSheet } from "react-native"
-import { Button, TextInput, View } from "react-native-web"
-import { Text } from "react-native-web"
+import { useState } from "react";
+import { StyleSheet } from "react-native";
+import { Button, Modal, TextInput, View } from "react-native-web";
+import { Text } from "react-native-web";
 import { createUserWithEmailAndPassword } from "firebase/auth";
-import { auth, db } from '../config';
-import {collection, addDoc} from 'firebase/firestore';
-
-
-
-
+import { auth, db } from "../config";
+import { collection, addDoc } from "firebase/firestore";
 
 export default function SignUp() {
-    const [username, setUsername] = useState("")
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-    const [buttonDisabled, setButtonDisabled] = useState(false)
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [buttonDisabled, setButtonDisabled] = useState(false);
+    const [modalVisible, setModalVisible] = useState(false);
 
+    function handleSubmit() {
+        setButtonDisabled(true);
+        setModalVisible(true);
 
-    const userData = {
-        uid: "",
-        username: "",
-        email: "",
-        }
-
-    function handleSubmit(){
-        userData.username = username;
-        userData.email = email;
-        console.log(userData)
-        setButtonDisabled(true)
-        
         createUserWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-            userData.uid = userCredential.user.uid;
-            const usersCollection = collection(db, 'users');
-
-      // Add a new document with a generated ID
-            addDoc(usersCollection, userData)
-            .then((docRef) => {
-                console.log('User document written with ID: ', docRef.id);
-
-                // Now you can navigate to the home page or perform other actions
-                
-
+            .then((userCredential) => {
+                // Signed up
+                const user = userCredential.user;
+                console.log(user.uid);
+                const userData = {
+                    uid: user.uid,
+                    username: username,
+                    email: email,
+                };
+                const usersCollection = collection(db, "users");
+                // Add a new document with a generated ID
+                addDoc(usersCollection, userData)
+                    .then((docRef) => {
+                        console.log(
+                            "User document written with ID: ",
+                            docRef.id
+                        );
+                        // Now you can navigate to the home page or perform other actions
+                    })
+                    .catch((error) => {
+                        console.error("Error adding document: ", error);
+                        // Handle Firestore error
+                    });
             })
             .catch((error) => {
-                console.error('Error adding document: ', error);
-                // Handle Firestore error
+                console.log(error);
             });
-        })
-        .catch((error) => {
-            console.log(error);
-        })
-
     }
 
-     //API CALL TO SEND THESE DETAILS TO FIREBASE
-        //SEPERATE PASSWORD API CALL TO STORE PASSWORD
-        
-        //Once all completed 
-
-
-
+    if (modalVisible) {
+        return (
+            <View style={styles.centeredView}>
+                <Modal visible={modalVisible} animationType="fade">
+                    <View style={styles.centeredView}>
+                        <Text>All signed up!</Text>
+                        <Button
+                            title="OK"
+                            onPress={setModalVisible(false)}
+                        ></Button>
+                    </View>
+                </Modal>
+            </View>
+        );
+    }
 
     return (
-
-        <View> 
-            <Text style={styles.text}>username</Text>
-        <TextInput
-            style={styles.textInput}
-            placeholder="Type here to translate!"
-            onChangeText={newText => setUsername(newText)}
-            defaultValue={username}
-            />
-            <Text style={styles.text}>email</Text>
-            <TextInput
-                style={styles.textInput}
-                placeholder="Type here to translate!"
-                onChangeText={newText => setEmail(newText)}
-                defaultValue={email}
-            />
-            <Text style={styles.text}>password</Text>
-            <TextInput
-            style={styles.textInput}
-            placeholder="Type here to translate!"
-            onChangeText={newText => setPassword(newText)}
-            defaultValue={password}
-            />
-
-            <Button style={styles.button} disabled={buttonDisabled} title="submit" onPress={handleSubmit}/>
-
-        </View>
+        <>
+            <View>
+                <Text style={styles.text}>username</Text>
+                <TextInput
+                    style={styles.textInput}
+                    placeholder="Type here to translate!"
+                    onChangeText={(newText) => setUsername(newText)}
+                    defaultValue={username}
+                />
+                <Text style={styles.text}>email</Text>
+                <TextInput
+                    style={styles.textInput}
+                    placeholder="Type here to translate!"
+                    onChangeText={(newText) => setEmail(newText)}
+                    defaultValue={email}
+                />
+                <Text style={styles.text}>password</Text>
+                <TextInput
+                    style={styles.textInput}
+                    placeholder="Type here to translate!"
+                    onChangeText={(newText) => setPassword(newText)}
+                    defaultValue={password}
+                />
+            </View>
+            <View>
+                <Button
+                    style={styles.button}
+                    disabled={buttonDisabled}
+                    title="submit"
+                    onPress={handleSubmit}
+                />
+            </View>{" "}
+        </>
     );
-
-
-
 }
 
-
 const styles = StyleSheet.create({
-    container: {flex: 5, backgroundColor: "black", color: "white", fontSize: 20, alignContent: 'space-between'},
-    textInput: {backgroundColor:"black", color:"white", height: 40, fontSize: 20},
-    text: {fontSize: 20},
-    button: {marginTop: 10}, 
+    container: {
+        flex: 5,
+        backgroundColor: "black",
+        color: "white",
+        fontSize: 20,
+        alignContent: "space-between",
+    },
+    textInput: {
+        backgroundColor: "black",
+        color: "white",
+        height: 40,
+        fontSize: 20,
+    },
+    centeredView: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 22,
+    },
 
-})
+    modalView: {
+        margin: 20,
+        backgroundColor: "white",
+        borderRadius: 20,
+        padding: 35,
+        alignItems: "center",
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5,
+    },
+    text: { fontSize: 20 },
+    button: { margin: 10 },
+});
