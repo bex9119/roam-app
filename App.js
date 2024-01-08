5365742fb7f37db272942bada00e0f466e3fefdc
rcastola <r.castola@gmail.com>
2024-01-08 11:46:24 +0000

    add button to add new landmark

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
-import { StyleSheet} from "react-native";
+import { StyleSheet } from "react-native";
 import SignUp from "./Screens/SignUp";
 import LoginPage from "./Screens/LoginPage";
-import Landmarks from "./Screens/Landmarks"
+import Landmarks from "./Screens/Landmarks";
 import WhatsLocal from "./Screens/WhatsLocal";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
@@ -9,29 +9,28 @@ import Routes from "./Screens/Routes";
 import MapScreen from "./Screens/MapScreen";
 import { useState } from "react";
 
-
 export default function App() {
-    const Stack = createNativeStackNavigator();
+  const Stack = createNativeStackNavigator();
 
-    return (          
-        <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen name="SignUp" component={SignUp}/>
-                <Stack.Screen name="LoginPage" component={LoginPage}/>
-                <Stack.Screen name="Landmark" component={Landmarks}/>
-                <Stack.Screen name="Routes" component={Routes}/>
-                <Stack.Screen name="WhatsLocal?" component={WhatsLocal}/>
-                <Stack.Screen name="MapScreen" component={MapScreen}/>
-            </Stack.Navigator>
-        </NavigationContainer>
-    );
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="SignUp" component={SignUp} />
+        <Stack.Screen name="LoginPage" component={LoginPage} />
+        <Stack.Screen name="MapScreen" component={MapScreen} />
+        <Stack.Screen name="Landmark" component={Landmarks} />
+        <Stack.Screen name="Routes" component={Routes} />
+        <Stack.Screen name="WhatsLocal?" component={WhatsLocal} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center",
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
--- Screens/MapScreen.js
+++ Screens/MapScreen.js
@@ -1,12 +1,16 @@
 import React, { useEffect, useState } from "react";
-import MapView, { Polyline, Polygon } from "../setup/map";
+import MapView, { Polyline, Polygon, Marker } from "../setup/map";
 import * as Location from "expo-location";
 import createGrid from "../utils/createGrid";
 import mapStyle from "../assets/mapStyle.json";
+import { Pressable, Text, View, StyleSheet } from "react-native";
+
 export default function MapScreen() {
   const [location, setLocation] = useState({});
   const [locationHistory, setLocationHistory] = useState([]);
   const [region, setRegion] = useState(createGrid());
+  const [addButtonClicked, setAddButtonClicked] = useState(false);
+
   useEffect(() => {
     const startLocationUpdates = () => {
       Location.requestForegroundPermissionsAsync().then(({ status }) => {
@@ -17,6 +21,7 @@ export default function MapScreen() {
         return Location.watchPositionAsync(
           { accuracy: Location.Accuracy.Highest, timeInterval: 5000 },
           (location) => {
+            console.log(location, "in use effect");
             const newCoordinates = {
               latitude: location.coords.latitude,
               longitude: location.coords.longitude,
@@ -50,34 +55,90 @@ export default function MapScreen() {
       return updatedRegion;
     });
   }, [location]);
+
+  function addPinFunction() {
+    console.log(location, "in new function");
+    setAddButtonClicked(true);
+  }
   return (
-    <MapView
-      minZoomLevel={12}
-      maxZoomLevel={20}
-      style={{ flex: 1 }}
-      initialRegion={{
-        latitude: 53.8,
-        longitude: -1.54,
-        latitudeDelta: 0.09,
-        longitudeDelta: 0.04,
-      }}
-      provider="google"
-      googleMapsApiKey="AIzaSyBdvF-tHDZd-CAjetSae6Eut8VL_xrgpMw"
-      customMapStyle={mapStyle}
-    >
-      {region.map((tile, index) => {
-        return (
-          <Polygon
-            key={`tile${index}`}
-            coordinates={tile.location}
-            fillColor={
-              tile.fill ? "rgba(105,105,105,1)" : "rgba(105,105,105,0)"
-            }
-            strokeColor="rgba(0,0,0,0)"
-          />
-        );
-      })}
-      {location && <Polyline coordinates={locationHistory} strokeWidth={5} />}
-    </MapView>
+    <View style={styles.container}>
+      <View style={styles.mapView}>
+        <MapView
+          minZoomLevel={7}
+          maxZoomLevel={20}
+          style={{ flex: 1, height: "100%" }}
+          initialRegion={{
+            latitude: 53.8,
+            longitude: -1.54,
+            latitudeDelta: 0.09,
+            longitudeDelta: 0.04,
+          }}
+          provider="google"
+          googleMapsApiKey="AIzaSyBdvF-tHDZd-CAjetSae6Eut8VL_xrgpMw"
+          customMapStyle={mapStyle}
+        >
+          {region.map((tile, index) => {
+            return (
+              <Polygon
+                key={`tile${index}`}
+                coordinates={tile.location}
+                fillColor={
+                  tile.fill ? "rgba(105,105,105,1)" : "rgba(105,105,105,0)"
+                }
+                strokeColor="rgba(0,0,0,0)"
+              />
+            );
+          })}
+          {location && (
+            <Polyline coordinates={locationHistory} strokeWidth={5} />
+          )}
+          {addButtonClicked && (
+            <Marker
+              coordinate={{
+                latitude: location.latitude,
+                longitude: location.longitude,
+              }}
+            />
+          )}
+        </MapView>
+      </View>
+      <Pressable style={styles.addPinButton} onPress={addPinFunction}>
+        <Text>Add a new landmark</Text>
+      </Pressable>
+    </View>
   );
 }
+
+// <Marker
+//           onPress={() => {
+//             {
+//               console.log("marker clicked");
+//               console.log(data.id, "index");
+//               navigation.navigate("Landmark", { id: data.id });
+//             }
+//           }}
+//           key={data.id}
+//           coordinate={{
+//           latitude: data.Coordinate._lat,
+//           longitude: data.Coordinate._long,
+//           }}
+//           title={`${data.Title}`}
+//            description={`${data.Description}`}
+//        />
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  mapView: {
+    height: "90%",
+    width: "100%",
+  },
+  addPinButton: {
+    padding: 20,
+    margin: 10,
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: "#f0f0f0",
+    backgroundColor: "#2596be",
+  },
+});
