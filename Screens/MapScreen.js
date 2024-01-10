c2d4900a12a365099671c653121cc2f941adc8f5
bex9119 <60200396+bex9119@users.noreply.github.com>
2024-01-10 10:00:12 +0000

    add loading modal to map

--- Screens/MapScreen.js
+++ Screens/MapScreen.js
@@ -13,7 +13,7 @@ import {
 } from "firebase/firestore";
 import { db } from "../config";
 import Modal from "react-native-modal";
-import { Pressable, Text, View, StyleSheet, TextInput } from "react-native";
+import { Pressable, Text, View, StyleSheet, TextInput, Image, TouchableOpacity, ActivityIndicator } from "react-native";
 import { useNavigation } from "@react-navigation/core";
 
 export default function MapScreen() {
@@ -25,8 +25,8 @@ export default function MapScreen() {
   const [addButtonClicked, setAddButtonClicked] = useState(false);
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [newLandmarkTitle, setNewLandmarkTitle] = useState("");
-  const [isLoading, setIsLoading] = useState(true);
   const [geoHistory, setGeoHistory] = useState([]);
+  const [loadingModal, setLoadingModal] = useState(true)
 
   function loadMaps() {
     return getDoc(doc(db, "Maps", "HJLCbJGvssb2onQTbiy4")).then((snapshot) => {
@@ -69,6 +69,7 @@ export default function MapScreen() {
     startLocationUpdates();
   }, []);
 
+
   useEffect(() => {
     setRegion((currRegion) => {
       const updatedRegion = currRegion.map((area) => {
@@ -102,7 +103,7 @@ export default function MapScreen() {
       })
       .then((array) => {
         setFinalLandmarkArray(array);
-        setIsLoading(false);
+        
       });
   }, [newLandmarkTitle]);
 
@@ -128,88 +129,112 @@ export default function MapScreen() {
       });
   }
 
-  return (
-    <View style={styles.container}>
-      <View style={styles.mapView}>
-        <MapView
-          minZoomLevel={5}
-          style={{ flex: 1, height: "100%" }}
-          initialRegion={{
-            latitude: 53.8,
-            longitude: -1.54,
-            latitudeDelta: 0.09,
-            longitudeDelta: 0.04,
-          }}
-          provider="google"
-          googleMapsApiKey={loadMaps}
-          customMapStyle={mapStyle}
-        >
-          {region.map((tile, index) => {
-            return (
-              <Polygon
-                key={`tile${index}`}
-                coordinates={tile.location}
-                fillColor={
-                  tile.fill ? "rgba(208,208,208,1)" : "rgba(208,208,208,0)"
-                }
-                strokeColor="rgba(0,0,0,0)"
-              />
-            );
-          })}
-          {location && (
-            <Polyline coordinates={locationHistory} strokeWidth={5} />
-          )}
+    const delay = (time) => new Promise((resolve) => setTimeout(resolve, time));
+    delay(5000).then(() => setLoadingModal(false));
 
-          {finalLandmarkArray.map((data, index) => (
-            <Marker
-              onPress={() => {
-                {
-                  navigation.navigate("Landmarks", { id: data.id });
-                }
-              }}
-              key={index}
-              coordinate={{
-                latitude: data.Coordinate._lat,
-                longitude: data.Coordinate._long,
-              }}
-              title={`${data.Title}`}
-              description={`${data.Description}`}
-            />
-          ))}
-        </MapView>
-      </View>
-      {addButtonClicked && (
-        <View style={styles.separator}>
-          <Modal isVisible={isModalVisible}>
-            <View style={styles.modal}>
-              <TextInput
-                placeholder="What Landmark is this?"
-                style={styles.textInput}
-                onChangeText={(title) => setNewLandmarkTitle(title)}
-              />
-              <Pressable style={styles.addPinButton} onPress={submitLandmark}>
-                <Text>Submit</Text>
-              </Pressable>
-            </View>
-          </Modal>
+return (
+  <View style={styles.container}>
+    <View
+      style={{
+        flex: 1,
+      }}
+    >
+      <Modal
+        isVisible={loadingModal}
+        transparent={false}
+        style={styles.content}
+      >
+        <View>
+          <Image
+            source={require("../assets/Landmark.png")}
+            style={{
+              height: 200,
+              width: 100,
+              padding:75
+            }}
+          />
+          <ActivityIndicator size="large" />
+          <Text style={styles.text}>Explore your local area</Text>
+          <Text style={styles.text}>Discover new places</Text>
+          <Text style={styles.text}>Share your favourite spots</Text>
         </View>
-      )}
-
-      <Pressable style={styles.addPinButton} onPress={addPinFunction}>
-        <Text>Add a new landmark</Text>
-      </Pressable>
+      </Modal>
     </View>
-  );
-}
+    <View style={styles.mapView}>
+      <MapView
+        minZoomLevel={15}
+        style={{ flex: 1, height: "100%" }}
+        initialRegion={{
+          latitude: 53.82,
+          longitude: -1.58,
+          latitudeDelta: 0.09,
+          longitudeDelta: 0.04,
+        }}
+        provider="google"
+        googleMapsApiKey={loadMaps}
+        customMapStyle={mapStyle}
+      >
+        {region.map((tile, index) => {
+          return (
+            <Polygon
+              key={`tile${index}`}
+              coordinates={tile.location}
+              fillColor={
+                tile.fill ? "rgba(208,208,208,1)" : "rgba(208,208,208,0)"
+              }
+              strokeColor="rgba(0,0,0,0)"
+            />
+          );
+        })}
+        {location && <Polyline coordinates={locationHistory} strokeWidth={5} />}
 
-// <Image
-// source="require(../../../assets/cyclist-icon.png)"
-// style={styles.markerImage}
-// />
+        {finalLandmarkArray.map((data, index) => (
+          <Marker
+            onPress={() => {
+              {
+                navigation.navigate("Landmarks", { id: data.id });
+              }
+            }}
+            key={index}
+            coordinate={{
+              latitude: data.Coordinate._lat,
+              longitude: data.Coordinate._long,
+            }}
+            title={`${data.Title}`}
+            description={`${data.Description}`}
+          >
+          </Marker>
+        ))}
+      </MapView>
+    </View>
+    {addButtonClicked && (
+      <View style={styles.separator}>
+        <Modal isVisible={isModalVisible}>
+          <View style={styles.modal}>
+            <TextInput
+              placeholder="What Landmark is this?"
+              style={styles.textInput}
+              onChangeText={(title) => setNewLandmarkTitle(title)}
+            />
+            <Pressable style={styles.addPinButton} onPress={submitLandmark}>
+              <Text>Submit</Text>
+            </Pressable>
+          </View>
+        </Modal>
+      </View>
+    )}
+
+    <Pressable style={styles.addPinButton} onPress={addPinFunction}>
+      <Text>Add a new landmark</Text>
+    </Pressable>
+  </View>
+); 
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "rgba(255, 255, 255, 1)",
   },
   separator: {
     marginVertical: 30,
@@ -245,6 +270,13 @@ const styles = StyleSheet.create({
     borderRadius: 25,
     borderWidth: 1,
     borderColor: "#ffffff",
+    justifyContent: "center",
+  },
+  text: {
+    color: "white",
+  },
+  content: {
+    alignItems: "center",
     justifyContent: "center",
   },
 });
